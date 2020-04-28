9b935143bd22009d490ec0818ff4b009286cc840
George Dalianis <georgedalianis@Georges-MBP-2.attlocal.net>
2020-04-27 21:23:30 -0500

    still working

--- app/app.R
+++ app/app.R
@@ -20,7 +20,7 @@ education <- read_rds("education_data.rds")
 ui <- fluidPage(theme = shinytheme("cosmo"),
   titlePanel("Social Spending Programs in the OECD"),
   tabsetPanel(
-    tabPanel("About",
+    tabPanel("OECD Overview",
              h2("What is the OECD?"),
              p("Founded in 1961, the Organization for Economic Co-operation and
                 Development (OECD) is an intergovernmental economic organization
@@ -39,7 +39,7 @@ ui <- fluidPage(theme = shinytheme("cosmo"),
                    Canada and the United States both joined by signing the new OECD Convention.
                    Other nations began to join, sparked by Japan's entry in 1964."),
                  plotlyOutput("rich_v_poor"))),
-    tabPanel("Overview", 
+    tabPanel("Social Spending: A Closer Look", 
            mainPanel(
              h3("Social Spending Intro"), 
              p("In this project, I am defining social spending as expenditure comprising in-cash benefits, 
@@ -49,7 +49,7 @@ ui <- fluidPage(theme = shinytheme("cosmo"),
                governments control financial flows, not private agents. Private transfers are not 
                included in these calculations."),
              plotOutput("image"),
-             h3("This graph is an demonstration of social spending in the OECD Countries"),
+             h3("OECD Development Over Time"),
              p("This graph examines the connection between GDP per capita and life expectancy. T
                he size of each circle in the graph indicates total population."),
              plotOutput("animation"))),
@@ -70,10 +70,25 @@ ui <- fluidPage(theme = shinytheme("cosmo"),
             A higher coefficient indicates greater inequality, as it indicates that 
             higher income individuals are receiving much larger percentages of the
             total income of the population. While the Gini can sometimes overstate
-            inequality, it is still an important component of a nation's income distribution."),
-          h2("Labor Market Freedom"),
+            inequality, it is still an important component of a nation's income 
+            distribution. The coefficient between inequality and social spending
+            displays a slight negative relationship, with a coefficient of -.004. This means that a 1% increase in social spending as a percent of GDP is associated with a .004 decline in the Gini coefficient. So increasing social spending programs is associated with less inequality."),
+          p(strong("GDP Per Capita:"), "Correlation Coefficient of 830.05. A 1% increase in social spending as a % of GDP is associated with an increase in GDP per capita by about $830."),
+          h2("Labor Market Regulation"),
           plotOutput("labor_market_graph"),
-          p("Analysis of Coefficient and stuff"))),
+          p("Analysis of Coefficient and stuffadadsfkjhadsfkjhasdfkjh"))),
+    tabPanel("Social/Societal Factors",
+             sidebarLayout(
+               sidebarPanel(
+                 selectInput(
+                   inputId = "migration",
+                   label = "Select Input:",
+                   choices = c("Migration", "Unemployment Spending", "Pension Spending"), selected = "Migration")),
+               mainPanel(
+                 plotOutput("migration_graph")
+               )
+                 )
+                 ),
     tabPanel("About",
              h2("About the Data"),
              p("All data was sourced from the",
@@ -90,14 +105,24 @@ ui <- fluidPage(theme = shinytheme("cosmo"),
                a(href = "https://data.oecd.org/healthstat/life-expectancy-at-birth.htm", "economic inequality,"),
                "and",
                a(href = "https://data.oecd.org/eduatt/adult-education-level.htm", "education"),
-               "statistics."),
+               "statistics. The earliest year of data used in this project is 1995 
+               and the latest is 2016. New OECD member countries include Lithuania (2018), 
+               Latvia (2016), Slovenia (2010), and Israel(2010). Data from these
+               countries was excluded in certain graphs because they were not OECD 
+               members at the time studied in the graphs."),
              h2("Project Motivations"),
-             p("After taking coursework in comparative politics and economics, I became very interested in international economic growth."),
+             p("I'm interested in institutions/policies that either spur or hinder 
+               economic growth. By examining factors like GDP per capita, labor markets,
+               life expectancy, and their connection social spending, I was able to
+               understand the broader policies behind economic growth.
+               I first considered examining the conditions of a
+               single country, Greece, but I found the larger trends within OECD
+               member nations to be much more intriguing."),
              h2("About Me"),
-             p("My name is George Dalianis, and I am a freshman at Harvard College studying government and economics. The source code for this project on my", 
+             p("My name is George Dalianis, and I am a freshman at Harvard College studying government and economics. I hope that you enjoyed my project! The source code for this project on my", 
                a(href = "https://github.com/georgedalianis7", "Github"), "account. Contact me at gdalianis@college.harvard.edu.")
-    )
-  ))
+    )))
+  
            
 
 server <- function(input, output) {
@@ -156,7 +181,18 @@ server <- function(input, output) {
   output$labor_market_graph <- renderPlot({
     readRDS(file = "mark_regression.rds")
   })
-  
+  output$migration_graph <- renderPlot({
+    if(input$migration == "Unemployment Spending") {
+      readRDS(file = "mig_graph.rds")
+    }
+    else if(input$migration == "Pension Spending"){
+      readRDS(file = "pen_graph.rds")
+    }
+    else{
+      readRDS(file = "mig_2.rds")
+    }
+    
+  })
 }
 # Run the application 
 shinyApp(ui = ui, server = server)
